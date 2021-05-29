446d360f57aba7ef320b97cde870be1961e0541e
Patrick Jones <patrick.jones@auctane.com>
2021-05-29 16:34:54 -0500

    add Account migration

---
+++ src/core/accounts/index.ts
@@ -0,0 +1,57 @@
+import { getManager } from "typeorm"
+import { Account } from "../account"
+
+/**
+ * User repository helper
+ */
+const accountRepo = () => getManager().getRepository<Account>('Account');
+
+const accountCreate = async (
+    auth0UserId: string,
+    email: string
+): Promise<Account> => {
+    try {
+        const repo = accountRepo();
+
+        const { identifiers: [pk] } = await repo.insert({
+            email,
+            auth0UserId
+        })
+
+        return await accountFindByPrimaryKey((pk as unknown) as number);
+    } catch(error) {
+        throw error;
+    }
+}
+
+export const accountFindByPrimaryKey = async (pk: number): Promise<Account> => {
+    const repo = accountRepo();
+
+    return repo.findOne(pk);
+}
+
+export const accountFindByAuth0UserId = async (auth0UserId: string) => {
+    const repo = accountRepo();
+
+    try {
+        return await repo.findOneOrFail({
+            where: [{ auth0UserId: auth0UserId }]
+        });
+    } catch (error) {
+        throw error;
+    }
+
+}
+
+export const accountFindOrCreateByAuth0UserId = async (
+    auth0UserId: string,
+    email: string
+): Promise<Account> => {
+    const repo = accountRepo();
+
+    try {
+        return await accountFindByAuth0UserId(auth0UserId)
+    } catch(error) {
+        return await accountCreate(auth0UserId, email);
+    }
+}
