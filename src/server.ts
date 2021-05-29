3b84790d35e1063589b39d7e2fc97279197e8fc9
Patrick Jones <patrick.jones@auctane.com>
2021-05-29 00:20:56 -0500

    clean up server

--- src/server.ts
+++ src/server.ts
@@ -1,47 +1,53 @@
 import bodyParser from 'body-parser';
-import express, { Request, Response } from 'express';
+import express, { application, Request, Response } from 'express';
 import { createConnection } from 'typeorm';
 import { User, Users } from './core';
 
 const port = 3000;
 
-createConnection().then(connection => {
-    const app = express();
+const app = express();
 
-    app.use(bodyParser.json())
+/* Middleware */
+app.use(bodyParser.json())
 
-    const userRepository = connection.getRepository(User)
-    
-    app.get('/', (req: Request, res: Response) => {
-        res.send('Hello, World!');
-    })
-    app.get("/users", async function(req: Request, res: Response) {
-        const users = await Users.listAll();
-        res.json(users);
-    });
-    
-    app.get("/users/:id", async function(req: Request, res: Response) {
-        const results = await Users.findById(req.params.id);
-        return res.send(results);
-    });
-    
-    app.post("/users", async function(req: Request, res: Response) {
-        const user = await Users.create(req.body);
-        return res.send(user);
-    });
-    
-    app.put("/users/:id", async function(req: Request, res: Response) {
-        const user = await Users.update(req.params.id, req.body);
-        return res.send(user);
-    });
-    
-    app.delete("/users/:id", async function(req: Request, res: Response) {
-        const user = await Users.destroy(req.params.id);
+/* Routes */
+app.get("/users", async function(req: Request, res: Response) {
+    const users = await Users.listAll();
+    res.json(users);
+});
 
-        return res.send(user);
+app.get("/users/:id", async function(req: Request, res: Response) {
+    const results = await Users.findById(req.params.id);
+    return res.send(results);
+});
+
+app.post("/users", async function(req: Request, res: Response) {
+    const user = await Users.create(req.body);
+    return res.send(user);
+});
+
+app.put("/users/:id", async function(req: Request, res: Response) {
+    const user = await Users.update(req.params.id, req.body);
+    return res.send(user);
+});
+
+app.delete("/users/:id", async function(req: Request, res: Response) {
+    const user = await Users.destroy(req.params.id);
+
+    return res.send(user);
     });
-    
+
+/* Server */
+async function start() {
+    try {
+        await createConnection();
+    } catch(e) {
+        throw e;
+    }
+
     app.listen(port, () => {
         console.log(`🚀 Server is listening on port ${port}`)
     });
-})
+}
+
+void start();
