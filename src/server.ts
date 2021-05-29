521a8bdb5ad641786aaf1890bc94c3ecf7babf65
Patrick Jones <patrick.jones@auctane.com>
2021-05-29 14:08:20 -0500

    add auth0

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import { ApolloServer } from 'apollo-server-express';
 import { createConnection } from 'typeorm';
 import schema from "./graphql/schema";
 import logger from "morgan";
+import { auth, requiresAuth } from "express-openid-connect";
 
 const port = 3000;
 
@@ -21,6 +22,20 @@ app.use(express.json());
 app.use("*", cors());
 
 app.use(logger('dev'));
+
+app.use(auth({
+    authRequired: false,
+    auth0Logout: true,
+    baseURL: 'http://localhost:3000',
+    clientID: 'tXKb5prH3WZLs2QMab2z7996ylkSxNKa',
+    issuerBaseURL: 'https://pmjones88.us.auth0.com',
+    secret: '_xGoOqy0PR4ZVIZk4s38uw_OcDWr2Lm3eFkZC8JQ7LovZb9e4zl6rIaZa1zkAf_n'
+}));
+
+app.get('/', requiresAuth(), (req, res) => {
+    console.log(req.oidc.isAuthenticated);
+    res.send(req.oidc.user);
+})
 
 server.applyMiddleware({ app, cors: true });
 
